b7880ef2856c191a51efbed4a3159801d9582290
Peter Bertel <peterbertel@gmail.com>
2020-07-01 18:03:30 -0400

    Update the user's email and password in auth.dart
    Create functions allowing the user to update their email and password in auth.dart. Also create a reusable method, setAuthServiceUserData, to set the variables relating to the logged-in user's information. Also delete some old comments in auth.dart.

--- lib/auth.dart
+++ lib/auth.dart
@@ -5,19 +5,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService with ChangeNotifier {
   var currentUser;
   var currentUserId;
+  var currentUserEmail;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<FirebaseUser> getUser() {
     return _auth.currentUser();
   }
 
-  /*
-   * ADD FIREBASE
-   *  1. Firebase for Flutter
-   * https://codelabs.developers.google.com/codelabs/flutter-firebase/index.html?index=..%2F..index#5
-   *  2. Finish intergration in the project as described in
-   * https://dev.to/aaronksaunders/simple-firebase-login-flow-in-flutter-now-firebase-23nk
-   */
+  void setAuthServiceUserData(FirebaseUser user) {
+    this.currentUser = user;
+    this.currentUserId = user.uid;
+    this.currentUserEmail = user.email;
+  }
 
   Future logout() async {
     var result = FirebaseAuth.instance.signOut();
@@ -25,22 +24,13 @@ class AuthService with ChangeNotifier {
     return result;
   }
 
-  // wrapping the firebase calls
-  // Future createUser(
-  //     {String firstName,
-  //       String lastName,
-  //       String email,
-  //       String password}) async {}
-
   Future<FirebaseUser> loginUser({String email, String password}) async {
     try {
       AuthResult result = await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password);
       notifyListeners();
       FirebaseUser user = result.user;
-      this.currentUser = user;
-      this.currentUserId = user.uid;
-
+      this.setAuthServiceUserData(user);
       return user;
     } catch (e) {
       throw new AuthException(e.code, e.message);
@@ -53,11 +43,19 @@ class AuthService with ChangeNotifier {
         .createUserWithEmailAndPassword(email: email, password: password);
       notifyListeners();
       FirebaseUser user = result.user;
-      this.currentUser = user;
-      this.currentUserId = user.uid;
+      this.setAuthServiceUserData(user);
       return user;
     } catch (e) {
       throw new AuthException(e.code, e.message);
     }
   }
+}
+
+void updateEmail(String email) async {
+  final result = this.currentUser.updateEmail(email);
+  this.currentUserEmail = email;
+}
+
+void updatePassword(String password) async {
+  final result = this.currentUser.updatePassword(password);
 }
